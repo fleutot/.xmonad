29c53e87eba7e9513b516d8ccc7afb6ebbc2ce1c
Gauthier Östervall <gauthier.ostervall@cipherstone.com>
2014-10-14 12:16:25 +0200

    Named workspaces, keyboard layout changer

--- xmonad.hs
+++ xmonad.hs
@@ -5,10 +5,13 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+myWorkspaces = ["1|edit", "2|term", "3|mail", "4|chat", "5|www", "6|priv", "7|media"]
+
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/gauthier/.xmonad/.xmobarrc"
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
+    xmonad $ defaultConfig {
+        workspaces = myWorkspaces
+        , manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
@@ -19,5 +22,6 @@ main = do
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
-        ]
+        , ((mod1Mask, xK_Shift_L), spawn "/home/gauthier/bin/layout_switch.sh")
+     ]
 
