98fad6730206f85ebab8208f6d64c5ee57183370
Gauthier Östervall <gauthier.ostervall@cipherstone.com>
2014-10-15 13:31:04 +0200

    Keyboard layout display, leave place for tray

--- xmonad.hs
+++ xmonad.hs
@@ -5,7 +5,7 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-myWorkspaces = ["1|edit", "2|term", "3|mail", "4|chat", "5|www", "6|priv", "7|media"]
+myWorkspaces = ["1|edit", "2|term", "3|mail", "4|chat", "5|www", "6|priv", "7|media", "8|admin"]
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/gauthier/.xmonad/.xmobarrc"
@@ -22,5 +22,4 @@ main = do
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
-        , ((mod1Mask, xK_Shift_L), spawn "/home/gauthier/bin/layout_switch.sh")
-     ]
+        ]
