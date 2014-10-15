9f89cb892346614f5acfcc3f0cd33529bbc56fc3
Gauthier Östervall <gauthier.ostervall@cipherstone.com>
2014-10-15 16:41:08 +0200

    Borders

--- xmonad.hs
+++ xmonad.hs
@@ -6,6 +6,9 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 myWorkspaces = ["1.edit", "2.term", "3.mail", "4.chat", "5.www", "6.priv", "7.media", "8.admin"]
+myBorderWidth = 2
+myNormalBorderColor = "grey16"
+myFocusedBorderColor = "#dd4814"
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/gauthier/.xmonad/.xmobarrc"
@@ -22,8 +25,13 @@ main = do
                         , ppSep = "  ||  "
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , borderWidth = myBorderWidth
+        , normalBorderColor  = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         ]
+
+
