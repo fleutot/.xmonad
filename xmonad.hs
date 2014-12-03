b9aa3bd70d6b4495827e9b8a7eaa8f38617ce55e
Gauthier Östervall <gauthier.ostervall@cipherstone.com>
2014-12-03 10:27:13 +0100

    Attempt to IM grid, does not work.
    Startup programs only if they exist.
    More room for trayer.
    Kinesis variant of SE layout (needs to be installed in X).

--- xmonad.hs
+++ xmonad.hs
@@ -2,11 +2,14 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.Grid
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.IM as IM
 import XMonad.Util.Dzen
 import XMonad.Util.Run(spawnPipe, runProcessWithInput)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Data.Ratio ((%))
 
 myWorkspaces = [" 1.edit ", " 2.term ", " 3.doc ", " 4.mail ", " 5.www ", " 6.chat ", " 7.priv ", " 8.media ", " 9.admin "]
 myBorderWidth = 2
@@ -16,7 +19,14 @@ myManageHook = composeAll
              [ className =? "Skype" --> doShift "6.chat"
              , manageDocks
              ]
-myLayout = (ResizableTall 1 (3/100) (1/2) []) ||| Mirror tiled ||| Full
+
+-- skypeLayout = IM.withIM (1%7) (IM.Title "g.ostervall_cipherstone.com - Skype™") Grid
+skypeLayout = IM.withIM (1%7) skypeRoster Grid
+skypeRoster = (IM.Title "g.ostervall_cipherstone.com - Skype™")
+
+-- (ClassName "Skype") `And` (Not (Title "Options")) `And` (Not (Role "Chats")) `And` (Not (Role "CallWindowForm"))
+
+myLayout = (ResizableTall 1 (3/100) (1/2) []) ||| Mirror tiled ||| Full ||| skypeLayout
   where
      tiled   = Tall nmaster delta ratio
      nmaster = 1
@@ -58,6 +68,7 @@ main = do
         , ((mod4Mask, xK_u), focusUrgent)
         , ((mod4Mask .|. shiftMask, xK_h), sendMessage MirrorShrink)
         , ((mod4Mask .|. shiftMask, xK_l), sendMessage MirrorExpand)
+        , ((controlMask .|. mod1Mask, xK_equal), kill)
         ]
 
 
